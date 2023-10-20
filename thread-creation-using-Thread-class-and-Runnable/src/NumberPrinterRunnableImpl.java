5417b2ad23aea82efe13477b5f10b85340742edc
Karan Dhondbare <kdhondbare123@gmail.com>
2023-10-21 00:01:59 +0530

    updating ThreadExampleUsingThreadClass

---
+++ thread-creation-using-Thread-class-and-Runnable/src/NumberPrinterRunnableImpl.java
@@ -0,0 +1,22 @@
+public class NumberPrinterRunnableImpl implements Runnable {
+
+    NumberPrinter numberPrinter;
+
+    NumberPrinterRunnableImpl(NumberPrinter numberPrinter){
+        this.numberPrinter = numberPrinter;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inside run method " + Thread.currentThread().getName() + " called run() method!!!"+ "" +
+                "Thread state is : "+ Thread.currentThread().getState());
+        try {
+            Thread.sleep(2000);//Thread goes to Timed_Waiting, thread is waiting for finite amount of time
+            this.numberPrinter.printNumber();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Inside run method " + Thread.currentThread().getName() + " called run() method!!!"+ "" +
+                "Thread state is : "+ Thread.currentThread().getState() + " completed!!!");
+    }
+}
--- thread-creation-using-Thread-class-and-Runnable/src/ThreadExampleUsingRunnableInterface.java
+++ thread-creation-using-Thread-class-and-Runnable/src/ThreadExampleUsingRunnableInterface.java
@@ -1,23 +1,24 @@
-public class ThreadExampleUsingThreadClass {
+public class ThreadExampleUsingRunnableInterface {
     public static void main(String[] args) {
         System.out.println("main method start: "+ Thread.currentThread());
         NumberPrinter numberPrinter = new NumberPrinter();
 
-        Thread thread1 = new NumberPrinterThread(numberPrinter);
+        Runnable task1 = new NumberPrinterRunnableImpl(numberPrinter);
+        Thread thread1 = new Thread(task1);
         thread1.setName("Thread-1");
-        thread1.setPriority(Thread.MAX_PRIORITY);
-        Thread thread2 = new NumberPrinterThread(numberPrinter);
+
+        Runnable task2 = new NumberPrinterRunnableImpl(numberPrinter);
+        Thread thread2 = new Thread(task2);
         thread1.setName("Thread-2");
-        thread1.setPriority(Thread.NORM_PRIORITY);
-        Thread thread3 = new NumberPrinterThread(numberPrinter);
+
+        Runnable task3 = new NumberPrinterRunnableImpl(numberPrinter);
+        Thread thread3 = new Thread(task3);
         thread1.setName("Thread-3");
-        thread1.setPriority(Thread.MIN_PRIORITY);
 
         try{
             thread1.start();//thread1 is calling run method
             thread2.start();//thread2 is calling run method
             thread3.start();//thread3 is calling run method
-
         }catch (RuntimeException e){
             System.out.println("RuntimeTime exception occurred.");
             e.printStackTrace();
