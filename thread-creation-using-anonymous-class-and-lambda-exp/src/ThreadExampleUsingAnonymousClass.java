8eeb7b9494b95990cc2208fc2e528d400b57b14a
Karan Dhondbare <kdhondbare123@gmail.com>
2023-10-22 23:11:27 +0530

    Adding ThreadExampleUsingAnonymousClass.

---
+++ thread-creation-using-anonymous-class-and-lambda-exp/src/ThreadExampleUsingAnonymousClass.java
@@ -0,0 +1,14 @@
+public class ThreadExampleUsingAnonymousClass {
+
+    public static void main(String[] args) {
+
+        Runnable lambdaTask = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Say hello from " + Thread.currentThread().getName() + " and state is : " + Thread.currentThread().getState());
+            }
+        };
+        Thread thread = new Thread(lambdaTask);
+        thread.start();
+    }
+}
